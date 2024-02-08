eff13136ee7538538e38a0afd79bc97a8372e67e
Alessandro Genova <alessandro.genova@kitware.com>
2024-02-08 09:30:15 -0500

    Add a couple of transforms that use nrtk's perturbers

--- nrtk_explorer/library/nrtk_transforms.py
+++ nrtk_explorer/library/nrtk_transforms.py
@@ -1,10 +1,15 @@
-from typing import Any, Optional, Dict
+from typing import Any, Optional, Dict, Tuple
 
 from PIL.Image import Image
 
 from nrtk_explorer.library.transforms import ImageTransform, ParameterDescription
 
 from nrtk.impls.perturb_image.generic.cv2.blur import GaussianBlurPerturber
+from nrtk.impls.perturb_image.pybsm.perturber import PybsmPerturber, PybsmSensor, PybsmScenario
+import pybsm
+
+import numpy as np
+from PIL import Image as ImageModule
 
 
 class NrtkGaussianBlurTransform(ImageTransform):
@@ -36,27 +41,124 @@ class NrtkGaussianBlurTransform(ImageTransform):
         }
 
     def execute(self, input: Image) -> Image:
-        return self._perturber.perturb(input)
+        input_array = np.asarray(input)
+        output_array = self._perturber.perturb(input_array)
+
+        return ImageModule.fromarray(output_array)
+
+
+# Taken from the nrtk package tests
+def createSampleSensorandScenario() -> Tuple[PybsmSensor, PybsmScenario]:
+
+    name = "L32511x"
+
+    # telescope focal length (m)
+    f = 4
+    # Telescope diameter (m)
+    D = 275e-3
+
+    # detector pitch (m)
+    p = 0.008e-3
+
+    # Optical system transmission, red  band first (m)
+    optTransWavelengths = np.array([0.58 - 0.08, 0.58 + 0.08]) * 1.0e-6
+    # guess at the full system optical transmission (excluding obscuration)
+    opticsTransmission = 0.5 * np.ones(optTransWavelengths.shape[0])
+
+    # Relative linear telescope obscuration
+    eta = 0.4  # guess
+
+    # detector width is assumed to be equal to the pitch
+    wx = p
+    wy = p
+    # integration time (s) - this is a maximum, the actual integration time will be
+    # determined by the well fill percentage
+    intTime = 30.0e-3
+
+    # dark current density of 1 nA/cm2 guess, guess mid range for a silicon camera
+    darkCurrent = pybsm.darkCurrentFromDensity(1e-5, wx, wy)
+
+    # rms read noise (rms electrons)
+    readNoise = 25.0
+
+    # maximum ADC level (electrons)
+    maxN = 96000.0
+
+    # bit depth
+    bitdepth = 11.9
+
+    # maximum allowable well fill (see the paper for the logic behind this)
+    maxWellFill = 0.6
+
+    # jitter (radians) - The Olson paper says that its "good" so we'll guess 1/4 ifov rms
+    sx = 0.25 * p / f
+    sy = sx
+
+    # drift (radians/s) - again, we'll guess that it's really good
+    dax = 100e-6
+    day = dax
+
+    # etector quantum efficiency as a function of wavelength (microns)
+    # for a generic high quality back-illuminated silicon array
+    # https://www.photometrics.com/resources/learningzone/quantumefficiency.php
+    qewavelengths = np.array([0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1]) * 1.0e-6
+    qe = np.array([0.05, 0.6, 0.75, 0.85, 0.85, 0.75, 0.5, 0.2, 0])
+
+    sensor = PybsmSensor(
+        name,
+        D,
+        f,
+        p,
+        optTransWavelengths,
+        opticsTransmission,
+        eta,
+        wx,
+        wy,
+        intTime,
+        darkCurrent,
+        readNoise,
+        maxN,
+        bitdepth,
+        maxWellFill,
+        sx,
+        sy,
+        dax,
+        day,
+        qewavelengths,
+        qe,
+    )
+
+    altitude = 9000.0
+    # range to target
+    groundRange = 60000.0
+
+    scenario_name = "niceday"
+    # weather model
+    ihaze = 1
+    scenario = PybsmScenario(scenario_name, ihaze, altitude, groundRange)
+    scenario.aircraftSpeed = 100.0
+
+    return sensor, scenario
 
 
 class NrtkPybsmTransform(ImageTransform):
-    def __init__(self, perturber=None):
-        # self._perturber = perturber
-        pass
+    def __init__(self, perturber: Optional[PybsmPerturber] = None):
+        if perturber is None:
+            sensor, scenario = createSampleSensorandScenario()
+            perturber = PybsmPerturber(sensor=sensor, scenario=scenario)
+
+        self._perturber: PybsmPerturber = perturber
 
     def get_parameters(self) -> dict[str, Any]:
         return {
-            # "D": self._perturber.sensor.D,
-            # "f": self._perturber.sensor.f,
-            "D": 28.0,
-            "f": 50.0,
+            "D": self._perturber.sensor.D,
+            "f": self._perturber.sensor.f,
         }
 
     def set_parameters(self, params: Dict[str, Any]):
-        # self._perturber.sensor.D = params["D"]
-        # self._perturber.sensor.f = params["f"]
-        # self._perturber.metrics = pybsm.niirs(self.sensor, self.scenario)
-        pass
+        self._perturber.sensor.D = params["D"]
+        self._perturber.sensor.f = params["f"]
+        self._perturber.metrics = pybsm.niirs(self._perturber.sensor, self._perturber.scenario)
 
     def get_parameters_description(self) -> Dict[str, ParameterDescription]:
         aperture_description: ParameterDescription = {
@@ -82,7 +184,9 @@ class NrtkPybsmTransform(ImageTransform):
 
     def execute(self, input: Image, input_parameters=None) -> Image:
         if input_parameters is None:
-            input_parameters = {"img_gsd": 1}
+            input_parameters = {"img_gsd": 0.15}
 
-        # return self._perturber.perturb(input, input_parameters)
-        return input
+        input_array = np.asarray(input)
+        output_array = self._perturber.perturb(input_array, input_parameters)
+
+        return ImageModule.fromarray(output_array)

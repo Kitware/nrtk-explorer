b2eabaf77cba42e6b0c03170e6324e7c79973f15
Paul Elliott <paul.elliott@kitware.com>
2024-05-16 13:49:29 -0400

    refactor(image_list): use Getter for image_id

--- src/nrtk_explorer/app/ui/image_list.py
+++ src/nrtk_explorer/app/ui/image_list.py
@@ -1,4 +1,4 @@
-from trame.widgets import html, quasar
+from trame.widgets import html, quasar, client
 
 from nrtk_explorer.widgets.nrtk_explorer import ImageDetection
 
@@ -8,6 +8,8 @@ class ImageList(html.Div):
         super().__init__(classes="row content-start", style="height: 100%")
         with self:
             with html.Div(
+                # For each original image, show all kinds of images.
+                # Assume equal number of images in each image kind array.
                 v_for=("id, idx in get(image_kinds[0].image_id_key).value",),
                 key="id",
                 classes="col-xs-12 col-sm-6 col-md-4 col-xl-3 q-pa-xs",
@@ -15,19 +17,21 @@ class ImageList(html.Div):
             ):
                 with quasar.QCard(flat=True, bordered=True):
                     with html.Div(classes="row"):
-                        with html.Div(v_for=("set in image_kinds",), classes="col-6 q-pa-sm"):
-                            html.Div("{{set.kind}}", classes="text-caption text-center")
-                            ImageDetection(
-                                identifier=("get(set.image_id_key).value[idx]",),
-                                src=("get(get(set.image_id_key).value[idx]).value",),
-                                meta=("get(`${get(set.image_id_key).value[idx]}_meta`).value",),
-                                annotations=(
-                                    "get(`${get(set.image_id_key).value[idx]}_result`).value",
-                                ),
-                                categories=("annotation_categories",),
-                                selected=("(get(set.image_id_key).value[idx] == hovered_id)",),
-                                hover=(hover_fn, "[$event]"),
-                            )
+                        with html.Div(v_for=("kind in image_kinds",), classes="col-6 q-pa-sm"):
+                            with client.Getter(
+                                key_name="image_id",
+                                name=("get(kind.image_id_key).value[idx]",),
+                            ):
+                                html.Div("{{kind.readable}}", classes="text-caption text-center")
+                                ImageDetection(
+                                    identifier=("image_id",),
+                                    src=("get(image_id).value",),
+                                    meta=(r"get(`${image_id}_meta`).value",),
+                                    annotations=(r"get(`${image_id}_result`).value",),
+                                    categories=("annotation_categories",),
+                                    selected=("(image_id == hovered_id)",),
+                                    hover=(hover_fn, "[$event]"),
+                                )
             html.Div(
                 "No images selected",
                 v_else=True,

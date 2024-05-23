939da90316503a46b89c83bcd14551a092840d0c
Paul Elliott <paul.elliott@kitware.com>
2024-05-22 20:07:08 -0400

    feat(image_list): add table panel under tab

--- src/nrtk_explorer/app/ui/image_list.py
+++ src/nrtk_explorer/app/ui/image_list.py
@@ -5,7 +5,35 @@ from nrtk_explorer.widgets.nrtk_explorer import ImageDetection
 
 class ImageList(html.Div):
     def __init__(self, hover_fn=None):
-        super().__init__(classes="row content-start", style="height: 100%")
+        super().__init__(classes="col")
+        with self:
+            with quasar.QTabs(classes="row", v_model=("image_list_tab", "grid"), align="left"):
+                quasar.QTab(name="grid", label="Grid")
+                quasar.QTab(name="table", label="Table")
+
+            with quasar.QTabPanels(
+                v_if="get(image_kinds[0].image_ids_list).value.length > 0",
+                v_model=("image_list_tab", "grid"),
+                animated=True,
+            ):
+                with quasar.QTabPanel(name="grid"):
+                    ImageGrid(hover_fn=hover_fn)
+                with quasar.QTabPanel(name="table"):
+                    ImageTable(hover_fn=hover_fn)
+            html.Div(
+                "No images selected",
+                v_if="get(image_kinds[0].image_ids_list).value.length === 0 && !loading_images",
+                classes="text-h5 row flex-center q-my-md",
+            )
+            quasar.QInnerLoading(
+                showing=("loading_images", False),
+                label="Loading, transforming, and annotating images...",
+            )
+
+
+class ImageGrid(html.Div):
+    def __init__(self, hover_fn=None):
+        super().__init__(classes="row")
         with self:
             with html.Div(
                 # For each original image, show all kinds of images.
@@ -13,7 +41,6 @@ class ImageList(html.Div):
                 v_for=("id, idx in get(image_kinds[0].image_ids_list).value",),
                 key="id",
                 classes="col-xs-12 col-sm-6 col-md-4 col-xl-3 q-pa-xs",
-                v_if="get(image_kinds[0].image_ids_list).value.length > 0",
             ):
                 with quasar.QCard(flat=True, bordered=True):
                     with html.Div(classes="row"):
@@ -31,13 +58,70 @@ class ImageList(html.Div):
                                     selected=("(image_id == hovered_id)",),
                                     hover=(hover_fn, "[$event]"),
                                 )
-            html.Div(
-                "No images selected",
-                v_if="get(image_kinds[0].image_ids_list).value.length === 0 && !loading_images",
-                classes="text-h5 col-12 row flex-center",
-                style="height: 100%",
-            )
-            quasar.QInnerLoading(
-                showing=("loading_images", False),
-                label="Loading, transforming, and annotating images...",
-            )
+
+
+class ImageTable(html.Div):
+    def __init__(self, hover_fn=None):
+        super().__init__()
+        with self:
+            with quasar.QTable(
+                flat=True,
+                columns=(
+                    """[
+                        { name: 'id', label: 'ID', field: 'id', sortable: true },
+                        { name: 'original', label: 'Original Image', field: 'original' },
+                        { name: 'transformed', label: 'Transformed Image', field: 'transformed' },
+                        { name: 'width', label: 'Width', field: 'width', sortable: true },
+                        { name: 'height', label: 'Height', field: 'height', sortable: true },
+                    ]""",
+                ),
+                rows=(
+                    r"""get(image_kinds[0].image_ids_list).value.map(id => 
+                            {
+                                const meta = get(`${id}_meta`).value
+                                return {  
+                                    id: id.split('_').at(-1), 
+                                    original: id, 
+                                    transformed: `transformed_${id}`,
+                                    originalAnnotations: get(`${id}_result`).value,
+                                    transformedAnnotations: get(`transformed_${id}_result`).value,
+                                    ...meta,
+                                }
+                            })
+                        """,
+                ),
+                row_key="id",
+                rows_per_page_options=("[0]",),  # [0] means show all rows
+            ):
+                with html.Template(
+                    v_slot_body_cell_original=True,
+                    __properties=[
+                        ("v_slot_body_cell_original", "v-slot:body-cell-original='props'")
+                    ],
+                ):
+                    with quasar.QTd():
+                        ImageDetection(
+                            style="max-width: 10rem; float: inline-end;",
+                            identifier=("props.row.original",),
+                            src=("get(props.row.original).value",),
+                            annotations=("props.row.originalAnnotations",),
+                            categories=("annotation_categories",),
+                            selected=("(props.row.original == hovered_id)",),
+                            hover=(hover_fn, "[$event]"),
+                        )
+                with html.Template(
+                    v_slot_body_cell_transformed=True,
+                    __properties=[
+                        ("v_slot_body_cell_transformed", "v-slot:body-cell-transformed='props'")
+                    ],
+                ):
+                    with quasar.QTd():
+                        ImageDetection(
+                            style="max-width: 10rem; float: inline-end;",
+                            identifier=("props.row.transformed",),
+                            src=("get(props.row.transformed).value",),
+                            annotations=("props.row.transformedAnnotations",),
+                            categories=("annotation_categories",),
+                            selected=("(props.row.transformed == hovered_id)",),
+                            hover=(hover_fn, "[$event]"),
+                        )

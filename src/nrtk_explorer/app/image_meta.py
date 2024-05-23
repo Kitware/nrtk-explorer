43a704e09bd121087a5bbf7cbea46263594308f9
Paul Elliott <paul.elliott@kitware.com>
2024-05-23 17:41:24 -0400

    fix(image_meta): add distance to meta type dict

--- src/nrtk_explorer/app/image_meta.py
+++ src/nrtk_explorer/app/image_meta.py
@@ -8,6 +8,7 @@ def image_id_to_meta(image_id: DatasetId):
 
 
 class DatasetImageMeta(TypedDict):
+    distance: int  # Transformed Embedding Distance
     width: int
     height: int
 
@@ -16,15 +17,15 @@ PartialDatasetImageMeta = TypedDict(
     "PartialDatasetImageMeta", {**DatasetImageMeta.__annotations__}, total=False
 )
 
-IMAGE_META_DEFAULTS: DatasetImageMeta = {"width": 0, "height": 0}
+IMAGE_META_DEFAULTS: DatasetImageMeta = {"distance": 0, "width": 0, "height": 0}
 
 
-def update_image_meta(state, dataset_id: DatasetId, meta: PartialDatasetImageMeta):
+def update_image_meta(state, dataset_id: DatasetId, meta_patch: PartialDatasetImageMeta):
     meta_key = image_id_to_meta(dataset_id)
     current_meta = {}
     if state.has(meta_key) and state[meta_key] is not None:
         current_meta = state[meta_key]
-    state[meta_key] = {**IMAGE_META_DEFAULTS, **current_meta, **meta}
+    state[meta_key] = {**IMAGE_META_DEFAULTS, **current_meta, **meta_patch}
 
 
 def delete_image_meta(state, dataset_id: DatasetId):

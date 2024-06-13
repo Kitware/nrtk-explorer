5463730d3b2920e04a84e5621ac3e3a2420df676
Paul Elliott <paul.elliott@kitware.com>
2024-06-12 20:03:53 -0400

    refactor: fix typing on object_detector, nrtk_transforms
    and trame_utils

--- src/nrtk_explorer/app/trame_utils.py
+++ src/nrtk_explorer/app/trame_utils.py
@@ -1,6 +1,7 @@
-from typing import Dict, Hashable
+from typing import Hashable
+from trame_server.state import State
 
 
-def delete_state(state: Dict, key: Hashable):
+def delete_state(state: State, key: Hashable):
     if state.has(key) and state[key] is not None:
         state[key] = None

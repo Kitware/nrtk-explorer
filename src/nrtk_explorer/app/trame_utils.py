6fecf69b20747ba03e85e97f8f2d431c68124fc7
Paul Elliott <paul.elliott@kitware.com>
2024-07-04 14:57:58 -0400

    refactor(transforms): put state.loading_images in _update_images

--- src/nrtk_explorer/app/trame_utils.py
+++ src/nrtk_explorer/app/trame_utils.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Hashable
 from trame_server.state import State
 
@@ -5,3 +6,16 @@ from trame_server.state import State
 def delete_state(state: State, key: Hashable):
     if state.has(key) and state[key] is not None:
         state[key] = None
+
+
+class SetStateAsync:
+    def __init__(self, state: State):
+        self.state = state
+
+    async def __aenter__(self):
+        return self.state
+
+    async def __aexit__(self, exc_type, exc, tb):
+        self.state.flush()
+        await asyncio.sleep(0)
+        await asyncio.sleep(0)

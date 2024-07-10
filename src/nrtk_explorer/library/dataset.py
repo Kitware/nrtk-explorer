bc5b0ca3c73a6121f144a62e6cbc34ecc6e72089
Paul Elliott <paul.elliott@kitware.com>
2024-07-10 12:21:06 -0400

    fix(dataset): prefer implicit return types
    get_dataset() returns a CocoDataset, not a Dataset.

--- src/nrtk_explorer/library/dataset.py
+++ src/nrtk_explorer/library/dataset.py
@@ -1,41 +1,15 @@
-from typing import TypedDict, List
 import kwcoco
 
 
-class DatasetCategory(TypedDict):
-    id: int
-    name: str
-
-
-class DatasetImage(TypedDict):
-    id: int
-    file_name: str
-    width: int
-    height: int
-
-
-class DatasetAnnotation(TypedDict):
-    id: int
-    image_id: int
-    category_id: int
-    bbox: List[int]
-
-
-class Dataset(TypedDict):
-    categories: List[DatasetCategory]
-    images: List[DatasetImage]
-    annotations: List[DatasetAnnotation]
-
-
-def load_dataset(path: str) -> Dataset:
+def load_dataset(path: str):
     return kwcoco.CocoDataset(path)
 
 
-dataset_json: Dataset = {"categories": [], "images": [], "annotations": []}
+dataset_json: kwcoco.CocoDataset = kwcoco.CocoDataset()
 dataset_path: str = ""
 
 
-def get_dataset(path: str, force_reload=False) -> Dataset:
+def get_dataset(path: str, force_reload=False):
     global dataset_json, dataset_path
     if dataset_path != path or force_reload:
         dataset_path = path

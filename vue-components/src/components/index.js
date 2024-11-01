c4cecc68a7fad656c1dad19587c808ddad2bb068
Paul Elliott <paul.elliott@kitware.com>
2024-11-01 12:30:47 -0400

    feat(ImageDetection): use component from trame-annotations

--- vue-components/src/components/index.js
+++ vue-components/src/components/index.js
@@ -1,11 +1,9 @@
-import ImageDetection from './ImageDetection.vue'
 import ScatterPlot from './ScatterPlot.vue'
 import ParamsWidget from './ParamsWidget.vue'
 import FilterOptionsWidget from './FilterOptionsWidget.vue'
 import FilterOperatorWidget from './FilterOperatorWidget.vue'
 
 export default {
-  imageDetection: ImageDetection,
   scatterPlot: ScatterPlot,
   paramsWidget: ParamsWidget,
   filterOptionsWidget: FilterOptionsWidget,

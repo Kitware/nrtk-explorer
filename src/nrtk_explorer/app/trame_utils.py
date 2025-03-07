fbcf25304d84215420d991702a243d16f5e0a6bf
Alessandro Genova <alessandro.genova@kitware.com>
2025-03-07 11:19:18 -0500

    feat(app): make it possible to enable/disable various features of the app

--- src/nrtk_explorer/app/trame_utils.py
+++ src/nrtk_explorer/app/trame_utils.py
@@ -1,4 +1,5 @@
 from typing import Hashable, Callable
+from trame_server import Server
 from trame_server.state import State
 
 
@@ -31,3 +32,42 @@ def change_checker(state: State, key: str, trigger_check=lambda a, b: a != b):
         return callback
 
     return decorator
+
+
+class ProcessingStep:
+    def __init__(
+        self,
+        server: Server,
+        feature_enabled_state_key: str,
+        gui_switch_key: str,
+        column_name: str,
+        enabled_callback: Callable,
+    ):
+        self.state = server.state
+        self.feature_enabled_state_key = feature_enabled_state_key
+        self.gui_switch_key = gui_switch_key
+        self.enabled_callback = enabled_callback
+        self.column_name = column_name
+        self.state.change(self.gui_switch_key)(self.on_gui_switch)
+        self.update_feature_enabled_state()
+        self.state.change("visible_columns", self.gui_switch_key)(
+            self.update_feature_enabled_state
+        )
+        self.state.change(self.feature_enabled_state_key)(self.on_change_feature_enabled)
+
+    def on_gui_switch(self, **kwargs):
+        if self.state[self.gui_switch_key]:
+            self.state.visible_columns = list(set([*self.state.visible_columns, self.column_name]))
+        else:
+            self.state.visible_columns = [
+                col for col in self.state.visible_columns if col != self.column_name
+            ]
+
+    def update_feature_enabled_state(self, **kwargs):
+        self.state[self.feature_enabled_state_key] = (
+            self.column_name in self.state.visible_columns and self.state[self.gui_switch_key]
+        )
+
+    def on_change_feature_enabled(self, **kwargs):
+        if self.state[self.feature_enabled_state_key]:
+            self.enabled_callback()
